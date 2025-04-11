a91f0bbaac4ffff1fc3ed75ede290f587dc4cfd4
holance <lunci.app@gmail.com>
2025-04-11 10:58:36 -0700

    Update unit tests and readme.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # ktbus
-A simple EventBus implementation based on Kotlin SharedFlow.
+A simple EventBus implementation based on Kotlin SharedFlow and inspired by 
+[Greenrobot EventBus](https://github.com/greenrobot/EventBus).
+
+# Usage
+
+Getting started:
+
+```kotlin
+data class Event1(val value: Int)
+data class Event2(val value: Int)
+data class Event3(val value: Int)
+
+class SomeClass {
+    val bus = KtBus.getDefault()
+
+    fun setup() {
+        bus.subscribe(this)
+    }
+
+    fun tearDown() {
+        bus.unsubscribe(this)
+    }
+
+    @Subscribe
+    fun onEvent1(event: Event1) {
+        // Do something with the event
+    }
+
+    @Subscribe
+    fun onEvent2(event: Event2) {
+        // Do something with the event
+    }
+
+    @Subscribe
+    fun onEvent3(event: Event3) {
+        // Do something with the event
+    }
+}
+
+val bus = KtBus.getDefault()
+
+bus.post(Event1(1))
+bus.post(Event2(2))
+bus.post(Event3(3))
+```
--- lib/src/test/kotlin/org/lunci/ktbus/KtBusTest.kt
+++ lib/src/test/kotlin/org/lunci/ktbus/KtBusTest.kt
@@ -25,6 +25,12 @@ class KtBusTest {
             assertEquals(i + 2, test.event3Result[i].value)
         }
         test.tearDown()
+        bus.post(Event1(1))
+        bus.post(Event2(2))
+        bus.post(Event3(3))
+        assertEquals(0, test.event1Result.size)
+        assertEquals(0, test.event2Result.size)
+        assertEquals(0, test.event3Result.size)
     }
 
     @Test
@@ -40,6 +46,32 @@ class KtBusTest {
         assertEquals(0, test.event1Result[0].value)
         assertEquals(1, test.event2Result[0].value)
         assertEquals(2, test.event3Result[0].value)
+        test.tearDown()
+        bus.removeStickyEvent(Event1::class.java)
+        bus.removeStickyEvent(Event2::class.java)
+        bus.removeStickyEvent(Event3::class.java)
+        test.setup()
+        assertEquals(0, test.event1Result.size)
+        assertEquals(0, test.event2Result.size)
+        assertEquals(0, test.event3Result.size)
+        test.tearDown()
+    }
+
+    @Test
+    fun channelTest() {
+        val iteration = 100
+        var test = TestClassWithChannel()
+        test.setup()
+        for (i in 0 until iteration) {
+            bus.post(Event1(i), TestClassWithChannel.CHANNEL_1)
+            bus.post(Event1(i + 1), TestClassWithChannel.CHANNEL_2)
+        }
+        assertEquals(iteration, test.eventCh1Result.size)
+        assertEquals(iteration, test.eventCh2Result.size)
+        for (i in 0 until iteration) {
+            assertEquals(i, test.eventCh1Result[i].value)
+            assertEquals(i + 1, test.eventCh2Result[i].value)
+        }
         test.tearDown()
     }
 }
@@ -62,6 +94,9 @@ class TestClass {
 
     fun tearDown() {
         bus.unsubscribe(this)
+        event1Result.clear()
+        event2Result.clear()
+        event3Result.clear()
     }
 
     @Subscribe
@@ -77,5 +112,49 @@ class TestClass {
     @Subscribe
     fun onEvent3(event: Event3) {
         event3Result.add(event)
+    }
+}
+
+@Suppress("unused")
+class TestClassWithChannel {
+    val bus = KtBus.getDefault()
+    companion object {
+        const val CHANNEL_1 = "channel 1"
+        const val CHANNEL_2 = "channel 2"
+    }
+
+    class TestChannel1 : ChannelFactory {
+        override fun createChannel(obj: Any): String {
+            return CHANNEL_1
+        }
+    }
+
+    class TestChannel2 : ChannelFactory {
+        override fun createChannel(obj: Any): String {
+            return CHANNEL_2
+        }
+    }
+
+    val eventCh1Result = mutableListOf<Event1>()
+    val eventCh2Result = mutableListOf<Event1>()
+
+    fun setup() {
+        bus.subscribe(this)
+    }
+
+    fun tearDown() {
+        bus.unsubscribe(this)
+        eventCh1Result.clear()
+        eventCh2Result.clear()
+    }
+
+    @Subscribe(channelFactory = TestChannel1::class)
+    fun onEventCh1(event: Event1) {
+        eventCh1Result.add(event)
+    }
+
+    @Subscribe(channelFactory = TestChannel2::class)
+    fun onEventCh2(event: Event1) {
+        eventCh2Result.add(event)
     }
 }
