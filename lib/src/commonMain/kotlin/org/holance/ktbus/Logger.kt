69535d3d0774799d8c4ff93facb0a373d47ac343
holance <holance.app@gmail.com>
2025-04-24 00:41:18 -0700

    Update Document.

--- lib/src/commonMain/kotlin/org/holance/ktbus/Logger.kt
+++ lib/src/commonMain/kotlin/org/holance/ktbus/Logger.kt
@@ -1,5 +1,17 @@
 package org.holance.ktbus
 
+/**
+ * Interface for logging messages at different levels of severity.
+ *
+ * This interface provides a standardized way to log messages with varying levels of detail,
+ * from debug information to critical errors. Implementors of this interface can define how
+ * these messages are handled, such as writing them to a file, console, or remote server.
+ *
+ * Each log method takes a lambda expression that returns a String. This allows for lazy
+ * evaluation of the log message, meaning the message is only constructed if the log level
+ * is enabled. This is particularly useful when constructing complex log messages that may
+ * involve string concatenation or other potentially expensive operations.
+ */
 @Suppress("unused")
 interface Logger {
     fun debug(message: () -> String)
